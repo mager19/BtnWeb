a1baacde456eba67175e34df5d8f6dd92a716ac0
Mario Germán Reyes C <mager19@gmail.com>
2018-09-13 16:01:28 -0500

    Opciones de texto para las imagenes

--- template-home.php
+++ template-home.php
@@ -19,6 +19,8 @@ get_template_part('template-parts/header', 'home'); ?>
 				$imagen_superior = get_theme_mod( 'imagen_superior' );
 				$imagen_abajoizquierda = get_theme_mod( 'imagen_abajoizquierda' );
 				$imagen_abajoderecha = get_theme_mod( 'imagen_abajoderecha' );
+				$texto_abajoizquierda = get_theme_mod( 'texto_abajoizquierda' );
+				$texto_abajoderecha = get_theme_mod( 'texto_abajoderecha' );
 
 			?>
 				<div class="col-md-12 with-background" style="background-image:url(<?php echo $imagen_superior; ?>);height: 90vh;">
@@ -26,14 +28,14 @@ get_template_part('template-parts/header', 'home'); ?>
 						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum laboriosam alias eveniet aut consequuntur, rem ut non eius, voluptatum repellendus repudiandae odio quos enim nisi commodi corporis ipsam? In, quas.
 					</div>
 				</div>
-				<div class="col-md-8 with-background" style="background-image:url(<?php echo $imagen_abajoizquierda; ?>);height: 75vh;">
+				<div class="col-md-8 with-background" style="background-image:url(<?php echo $imagen_abajoizquierda; ?>);">
 					<div class="header__left">
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis tempore, mollitia reiciendis veritatis quia tenetur sed! Odio earum vitae voluptate, perspiciatis consequatur fuga, ducimus vel nemo saepe excepturi soluta tempora.
+						<h3><?php echo $texto_abajoizquierda; ?></h3>
 					</div>	
 				</div>
-				<div class="col-md-4 with-background" style="background-image:url(<?php echo $imagen_abajoderecha; ?>);height: 75vh;">
+				<div class="col-md-4 with-background" style="background-image:url(<?php echo $imagen_abajoderecha; ?>);">
 					<div class="header__right">
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores delectus velit cupiditate, fugiat facere blanditiis nostrum ducimus iure omnis amet consequuntur repellendus corporis tempore optio enim impedit odio at incidunt.
+						<h3><?php echo $texto_abajoderecha; ?></h3>
 					</div>
 				</div>
 
