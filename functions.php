ff514ce8e97e5208a7cc40c7d40d81482d36b840
Mario Germán Reyes C <mager19@gmail.com>
2018-09-13 22:14:47 -0500

    añadido slick al final del home

--- functions.php
+++ functions.php
@@ -127,9 +127,9 @@ function batanaweb_scripts() {
 
 	wp_enqueue_style( 'fontawesome', 'https://use.fontawesome.com/releases/v5.3.1/css/all.css' );
 
-	// wp_enqueue_style( 'slickcss', get_template_directory_uri() . '/css/slick.css' );
+	wp_enqueue_style( 'slickcss', get_template_directory_uri() . '/css/slick.css' );
 
-	// wp_enqueue_style( 'slicktheme', get_template_directory_uri() . '/css/slick-theme.css' );
+	wp_enqueue_style( 'slicktheme', get_template_directory_uri() . '/css/slick-theme.css' );
 
 	wp_enqueue_style( 'woocommerce', get_template_directory_uri() . '/css/woocommerce.css' );
 		
@@ -137,7 +137,7 @@ function batanaweb_scripts() {
 
 	wp_enqueue_script( 'bootstrapjs', get_template_directory_uri() . '/js/bootstrap.js', array('jquery'), true );
 
-	// wp_enqueue_script( 'slickjs', get_template_directory_uri() . '/js/slick.min.js' );
+	wp_enqueue_script( 'slickjs', get_template_directory_uri() . '/js/slick.min.js' );
 
 	wp_enqueue_script( 'mainjs', get_template_directory_uri() . '/js/main.js' );
 
@@ -184,3 +184,6 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 if ( class_exists( 'WooCommerce' ) ) {
 	require get_template_directory() . '/inc/woocommerce.php';
 }
+
+
+remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_show_product_loop_sale_flash', 10 );
--- js/main.js
+++ js/main.js
@@ -1,4 +1,35 @@
 	jQuery(document).ready(function($){
 
+		$('.destacados__carrousel').slick({
+  				autoplay: false,
+  				speed: 2000,
+				dots: false,
+				infinite: true,
+				prevArrow:"<img class='a-left control-c prev slick-prev' src='../css/fonts/arrowleft.png'>",
+		    	slidesToShow: 5, 
+		  		slidesToScroll: 1,
+		  		arrows:false,
+		  		pauseOnHover: true,
+		  		responsive: [
+		  			
+				    {
+				      breakpoint: 800,
+				      settings: {
+				        slidesToShow: 1,
+				        slidesToScroll: 1,
+				        infinite: true,
+				        
+				      }
+		    		},    		
+		    		{
+		      		  breakpoint: 480,
+		      		  settings: {
+		        	  slidesToShow: 1,
+		        	  slidesToScroll: 1,
+		        	  arrows: false,
+		      		}
+		    },
+		    	]
+		});
 		
 	});
