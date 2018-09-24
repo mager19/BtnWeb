1e3ea3fc879fea59b6d8e31e4ca198cc624a7993
Avraxas <avraxas@gmail.com>
2018-09-24 16:06:20 -0500

    Modales para envío, devoluciones y pago a plazos

--- footer.php
+++ footer.php
@@ -51,15 +51,10 @@
       </div>
       <div class="modal-body">
       	<h4 style="text-align: center;">Envíos</h4>
-      	<p>ENVÍOS GRATUITOS para pedidos superiores a 100€ a cualquier parte del mundo. Los impuestos de aduana o costes ajenos al envío serán pagados por el comprador.</p>
+      	<?php get_theme_mod( 'textoEnvios' ); ?>
+      	
+      	</div>
 
-<p>España y Portugal (excluyendo Baleares, Canarias, Ceuta y Melilla): 5 euros
-Europa y resto de España: 18 euros <br>
-Resto del mundo: Pedir presupuesto por email <a href="mailto:pedidos@batanashop.com">pedidos@batanashop.com</a>
-Los costes de aduana no están incluidos en el precio y deberán ser pagados por el comprador.</p>
-
-<p>IMPORTANTE: En caso de devolución del producto se descontarán los gastos de envío a la hora de realizar la devolución del importe de la compra.</p></div>
-<h4 style="text-align: center;"><a href="#">¿Te gustaría pagar a plazos?</a></h4><br>
       
     </div>
   </div>
@@ -77,8 +72,27 @@ Los costes de aduana no están incluidos en el precio y deberán ser pagados por
       </div>
       <div class="modal-body">
       	<h4 style="text-align: center;">Devoluciones</h4>
-      	<p>Nuestro objetivo es tu total satisfacción como cliente. Para proceder al cambio o devolución de un artículo adquirido en la tienda online, deberás solicitarla a través de un email a pedidos@batanashop.com en un plazo no superior a 15 días naturales desde la recepción del producto, haciéndonos saber el motivo del cambio o devolución en caso que lo creas necesario para ayudarnos a seguir mejorando.</p>
-<p>Mas info en <a href="https://batana.snswebs.es/condiciones-de-venta/">"condiciones de venta"</a></p></div>
+      	<?php get_theme_mod( 'textoCambioDevoluciones' ); ?>
+      	</div>
+      
+    </div>
+  </div>
+</div>
+
+<!-- #pago a plazos Modal -->
+<div style="display: none;" class="modal fade" id="plazosModal" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">×</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      	<h4 style="text-align: center;">Devoluciones</h4>
+      	<?php get_theme_mod( 'textoPagoPlazos' ); ?>
+      	</div>
       
     </div>
   </div>
